1ab6f22a43ad51198238a1e8ce9add31c3dbe1ca
이기태 <91439717+kitaee@users.noreply.github.com>
2024-09-26 13:49:34 +0900

    [Bronze II] Title: 시험 감독, Time: 500 ms, Memory: 204220 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -2,33 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    static int N;
-    static Long[] tester;
-    static Long B,C;
+
+    static int N,B,C;
+    static Long[] arr;
     static Long answer = 0L;
-    
-	public static void main (String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    N = Integer.parseInt(br.readLine());
-	    tester = new Long[N];
-	    String[] info = br.readLine().split(" ");
-	    for(int i=0; i<N; i++) {
-	        tester[i] = Long.parseLong(info[i]);
-	    }
-	    info = br.readLine().split(" ");
-	    B = Long.parseLong(info[0]);
-	    C = Long.parseLong(info[1]);
-	    
-	    for(int i=0; i<N; i++) {
-	        tester[i]-=B;
-	        answer+=1;
-	        if(tester[i] > 0) {
-	           Double plus = Math.ceil(((double)tester[i]/C));
-	           answer+=plus.longValue();
-	        }
-	    }
-	    
-	    System.out.println(answer);
-	}
+
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        String[] info = br.readLine().split(" ");
+        arr = new Long[N];
+
+        for(int i=0; i<N; i++) {
+            arr[i] = Long.valueOf(info[i]);
+        }
+        info = br.readLine().split(" ");
+        B = Integer.parseInt(info[0]);
+        C = Integer.parseInt(info[1]);
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] -= B;
+            answer+=1;
+            if(arr[i] <= 0) {
+                continue;
+            } else {
+                answer += (long)(Math.ceil((double)arr[i]/C));
+            }
+        }
+        System.out.println(answer);
+    }
 }
