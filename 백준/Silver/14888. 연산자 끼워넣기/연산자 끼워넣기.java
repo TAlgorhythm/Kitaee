ce5e50b6fb062709579cadccc0a9252aeeb0c109
이기태 <91439717+kitaee@users.noreply.github.com>
2024-10-02 12:54:07 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -3,81 +3,47 @@ import java.util.*;
 
 public class Main {
 
-    static int N;
-    static int[] num;
-    static int[] calculate;
-    static int[] visited;
-    static int[] result;
     static int MAX = Integer.MIN_VALUE;
     static int MIN = Integer.MAX_VALUE;
+    static int N;
+    static String[] info;
+    static int[] nums;
+    static int[] operations = new int[4];
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        num = new int[N];
-        calculate = new int[N-1];
-        result = new int[N-1];
-        visited = new int[N-1];
-        int index = 0;
-
-        String[] info = br.readLine().split(" ");
+        nums = new int[N];
+        info = br.readLine().split(" ");
         for(int i=0; i<N; i++) {
-            num[i] = Integer.parseInt(info[i]);
+            nums[i] = Integer.parseInt(info[i]);
         }
         info = br.readLine().split(" ");
         for(int i=0; i<4; i++) {
-            int count = Integer.parseInt(info[i]);
-            for(int j=0; j<count; j++) {
-                calculate[index] = i;
-                index+=1;
-            }
+            operations[i] = Integer.parseInt(info[i]);
         }
-
-        permutation(0);
-        
+        dfs(nums[0], 1);
         System.out.println(MAX);
-        System.out.print(MIN);
+        System.out.println(MIN);
     }
 
-    static void permutation(int depth) {
-        if(depth == N-1) {
-            getCalculation();
+    static void dfs(int num, int index) {
+        if(index == N) {
+            MAX = Math.max(MAX, num);
+            MIN = Math.min(MIN, num);
             return;
         }
-
-        for(int i=0; i<N-1; i++) {
-            if(visited[i] == 0) {
-                visited[i] = 1;
-                result[depth] = calculate[i];
-                permutation(depth+1);
-                visited[i] = 0;
-            }
-        }
-    }
-
-    static void getCalculation() {
-        // num 배열과 calculate 배열이 들어있음
-        int temp = num[0];
-        int calculateIndex = 0;
-        for(int i=1; i<N; i++) {
-            if(result[calculateIndex] == 0) {
-                temp = temp + num[i];
-            } else if(result[calculateIndex] == 1) {
-                temp = temp - num[i];
-            } else if(result[calculateIndex] == 2) {
-                temp = temp * num[i];
-            } else {
-                if(temp > 0) {
-                    temp = temp / num[i];
-                } else {
-                    temp = temp * (-1);
-                    temp = temp / num[i];
-                    temp = temp * (-1);
+        for(int i=0; i<4; i++) {
+            if(operations[i] > 0) {
+                operations[i]-=1;
+                switch (i) {
+                    case 0: dfs(num + nums[index], index+1); break;
+                    case 1: dfs(num - nums[index], index+1); break;
+                    case 2: dfs(num * nums[index], index+1); break;
+                    case 3: dfs(num / nums[index], index+1); break;
                 }
+                operations[i]+=1;
             }
-            calculateIndex+=1;
         }
-        MAX = Math.max(MAX, temp);
-        MIN = Math.min(MIN, temp);
     }
 }
